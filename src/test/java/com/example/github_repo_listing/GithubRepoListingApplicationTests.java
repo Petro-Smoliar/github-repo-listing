c84069e211ee5f864c448f4ed3f9ca82d71d7944
Petr <smp22pl@gmail.com>
2024-07-19 10:50:28 +0200

    Initial project setup with Spring Boot dependencies

---
+++ src/test/java/com/example/github_repo_listing/GithubRepoListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_repo_listing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
