f4978a367a96b3b28fafd1fac7a977028977c665
Petr <smp22pl@gmail.com>
2024-07-20 14:41:59 +0200

    refactor: update Mapper and DTO

--- src/main/java/com/example/github_repo_listing/mapper/RepositoryMapper.java
+++ src/main/java/com/example/github_repo_listing/mapper/RepositoryMapper.java
@@ -4,8 +4,11 @@ import com.example.github_repo_listing.config.MapperConfig;
 import com.example.github_repo_listing.dto.RepositoryDto;
 import com.example.github_repo_listing.model.Repository;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface RepositoryMapper {
+
+    @Mapping(target = "owner", source = "owner.login")
     RepositoryDto toDto(Repository repository);
 }
